efefa287d7e165f5d2906d553c754cd57c509e7b
fares8lefi <fareslefi4@gmail.com>
2025-10-12 16:04:32 +0200

    create test controller

---
+++ src/Controller/DhiaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+final class DhiaController extends AbstractController
+{
+   
+}
